baa55f0d866c33eaff12b32debfd9e45b6c87745
Sierra McCahon <smitche4@mail.umw.edu>
2018-08-30 21:42:15 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,5 +36,9 @@ def self.find_by_name(name)
  def self.find_or_create_by_name(song_name)
     self.find_by_name(song_name) || self.create_by_name(song_name)
   end
+  
+  def self.alphabetical
+    @@all.sort_by! { |song| song.name }
+  end
 
 end 
