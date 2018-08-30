825cee88073a64dfc18620c2fffd750dd6d205d0
Sierra McCahon <smitche4@mail.umw.edu>
2018-08-30 21:37:57 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+class Song
+  attr_accessor :name, :artist_name
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def self.create
+    new_song = self.new
+    @@all << new_song
+    new_song
+  end
+
+  def self.new_by_name(song_name)
+    new_song = self.new
+    new_song.name = song_name
+    new_song
+  end
+end 
